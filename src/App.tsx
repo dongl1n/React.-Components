7f3febd57498eda00f4ebf749cb8ef444c8bef36
Juja <nastya341151@gmail.com>
2023-10-27 19:41:30 +0300

    feat: add new func

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, Component } from 'react'
+import Form from './handleSearch'
 import './App.css'
+import getPokemonCard from './pokemonApi'
 
-let GOMFL = "";
 function App() {
-  const [count, setCount] = useState(0)
+  const form = new Form();
+  getPokemonCard("harizard");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <section class="top">
+      <h1>What is <span>Pokémon?</span></h1>
+      <form class="search__container" onClick={form!.handleForm} onChange={form!.handleForm}>
+        <select name="selectedCardFilter">
+        <option value="all">All</option>
+          <option value="name">Name</option>
+          <option value="type">Type</option>
+        </select>
+        <input></input>
+        <button class="search"></button>
+      </form>
+    </section>
+    <section class ="bottom">
+    {getPokemonCard("Charizard")}
+    </section>
     </>
   )
 }
---
+++ src/pokemonApi.tsx
@@ -0,0 +1,13 @@
+import pokemon from 'pokemontcgsdk'
+
+pokemon.configure({apiKey: 'e6d53ce0-e9ce-4093-99c9-93b8badada32'})
+
+function getPokemonCard(value){
+  console.log("test")
+  pokemon.card.where({ q: `name:${value}` })
+  .then(card => {
+      return <div>{card}</div>
+  })
+}
+
+export default getPokemonCard;
