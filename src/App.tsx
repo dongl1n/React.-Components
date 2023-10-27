71967bca2891871efa8fc6f8377c591568dbf865
Juja <nastya341151@gmail.com>
2023-10-27 21:42:33 +0300

    feat: add ul for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import { useState, Component } from 'react'
 import Form from './handleSearch'
 import './App.css'
-import getPokemonCard from './pokemonApi'
+import {getAllCard, getPokemonCard} from './pokemonApi'
+
+function isEmpty(data){
+  if(data){
+    let listItems = data.map((dat) =>
+    <li>{dat.name}</li>)
+    return(
+      <ul>{listItems}</ul>
+    )
+  }
+  else return <span>test</span>;
+}
 
 function App() {
-  const form = new Form();
-  getPokemonCard("harizard");
+  const [items, setItems] = useState([]);
 
+  const form = new Form();
+  let res;
+  getPokemonCard("ditto").then((response) => {
+    setItems(response);
+  })
+  console.log(items.data)
+  
   return (
     <>
-    <section class="top">
-      <h1>What is <span>Pokémon?</span></h1>
-      <form class="search__container" onClick={form!.handleForm} onChange={form!.handleForm}>
-        <select name="selectedCardFilter">
-        <option value="all">All</option>
-          <option value="name">Name</option>
-          <option value="type">Type</option>
-        </select>
-        <input></input>
-        <button class="search"></button>
-      </form>
-    </section>
-    <section class ="bottom">
-    {getPokemonCard("Charizard")}
-    </section>
+      <section class="top">
+        <h1>What is <span>Pokémon?</span></h1>
+        <form class="search__container" onClick={form!.handleForm} onChange={form!.handleForm}>
+          <select name="selectedCardFilter">
+          <option value="all">All</option>
+            <option value="name">Name</option>
+            <option value="type">Type</option>
+          </select>
+          <input></input>
+          <button class="search"></button>
+        </form>
+      </section>
+      <section class ="bottom">
+      {isEmpty(items.data)}
+      </section>
     </>
   )
 }
--- src/pokemonApi.tsx
+++ src/pokemonApi.tsx
@@ -2,12 +2,18 @@ import pokemon from 'pokemontcgsdk'
 
 pokemon.configure({apiKey: 'e6d53ce0-e9ce-4093-99c9-93b8badada32'})
 
-function getPokemonCard(value){
-  console.log("test")
-  pokemon.card.where({ q: `name:${value}` })
+function getAllCard(){
+  return pokemon.card.all()
   .then(card => {
-      return <div>{card}</div>
+    return card;
   })
 }
 
-export default getPokemonCard;
+function getPokemonCard(value){
+  return pokemon.card.where({ q: `name:${value}` })
+  .then(card => {
+      return card;
+  })
+}
+
+export {getAllCard, getPokemonCard}
